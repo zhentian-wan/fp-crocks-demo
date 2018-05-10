a1eecf0b524cae1f9a69db13f3687c4980d29d76
Zhentian Wan <answer881215@gmail.com>
2018-05-10 22:24:47 +0300

    fix code

--- demo1.js
+++ demo1.js
@@ -10,7 +10,7 @@ const res2 = inc(undefined); // NaN
 */
 
 const safeNum = n => typeof n === 'number' ? Maybe.Just(n) : Maybe.Nothing();
-const input = safeNum('3'); // Just 3
+const input = safeNum(2); // Just 3
 // const input = safeNum('2'); // Nothing
 // const input = safeNum(undefined); // Nothing
 const result = input
