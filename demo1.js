17f1f697ecb7c3f0b0168ef7cbd7733ae957a9af
Zhentian Wan <answer881215@gmail.com>
2018-05-10 22:16:31 +0300

    Maybe type from crocks lib

---
+++ demo1.js
@@ -0,0 +1,20 @@
+const {inc} = require('./utils');
+const Maybe = require('crocks/Maybe');
+
+// Maybe.Just, Maybe.Nothing
+
+/*
+const res = inc(2); // 3
+const res1 = inc('2'); // 21
+const res2 = inc(undefined); // NaN
+*/
+
+const safeNum = n => typeof n === 'number' ? Maybe.Just(n) : Maybe.Nothing();
+const input = safeNum('3'); // Just 3
+// const input = safeNum('2'); // Nothing
+// const input = safeNum(undefined); // Nothing
+const result = input
+    .map(inc)
+    .option(0);
+
+console.log(result);
---
+++ utils.js
@@ -0,0 +1,6 @@
+
+const inc = n => n + 1;
+
+module.exports = {
+    inc
+};
